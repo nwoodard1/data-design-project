dfc5029f5318529a04c4e6ba227373adfdb52c47
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-16 16:17:01 -0700

    made all headers bold

--- epic/sabrinachavez.php
+++ epic/sabrinachavez.php
@@ -7,18 +7,17 @@
 	<body>
 		<img src="./sabrinachavez.jpg">
 		<h1> Sabrina Chavez </h1>
-		<p> Age:25</p>
-		<p>Race: Hispanic/Italian</p>
-		<p>About: Sabrina is a student at UNM and works part-time at Buffalo Exchange. She is passionate about fashion and clothing. She likes brands like Free People, Prada and Pucci.
+		<p> <strong>Age:</strong> 25</p>
+		<p> <strong>Race:</strong> Hispanic/Italian </p>
+		<p> <strong>About:</strong> Sabrina is a student at UNM and works part-time at Buffalo Exchange. She is passionate about fashion and clothing. She likes brands like Free People, Prada and Pucci.
 			Sabrina spends her free time shopping for deals online and in person.</p>
-		<p>Technology: Sabrina uses an iPhone 8 and a macbook pro laptop with macOS 10.14 mojave.
+		<p> <strong>Technology:</strong> Sabrina uses an iPhone 8 and a macbook pro laptop with macOS 10.14 mojave.
 		</p>
-		<p>Attitudes and Behaviors: Sabrina is a very social person, she goes to music festivals and local breweries. She uses Instagram and Snapchat, but doesn't have a Facebook profile.
+		<p> <strong> Attitudes and Behaviors:</strong> Sabrina is a very social person, she goes to music festivals and local breweries. She uses Instagram and Snapchat, but doesn't have a Facebook profile.
 		</p>
-		<p>Frustrations: Sabrina wants to be sure that whatever product she is ordering is not a knock-off. Sabrina wants to be sure that the Free People dress she wants to order is in good condition, with no holes or frayed edges. Sabrina wants to be sure that the product is priced lower than the original sale price.
+		<p> <strong> Frustrations:</strong> Sabrina wants to be sure that whatever product she is ordering is not a knock-off. Sabrina wants to be sure that the Free People dress she wants to order is in good condition, with no holes or frayed edges. Sabrina wants to be sure that the product is priced lower than the original sale price.
 		</p>
-		<p>Goals: Sabrina's goal is to find the highest quality Free People smock dress for the cheapest price.
+		<p><strong> Goals:</strong> Sabrina's goal is to find the highest quality Free People smock dress for the cheapest price.
 		</p>
-		<p>
-			User Story: As a user, I need to search for and filter options to find a Free People smock style dress.
+		<p><strong>User Story:</strong> As a user, I need to search for and filter options to find a Free People smock style dress.
 		</p>
--- epic/use-case.php
+++ epic/use-case.php
@@ -8,17 +8,17 @@
 	<body>
 		<p>
 		<h1><strong> User case </strong></h1>
-			Title: Purchasing products from Etsy.com
+			<strong>Title: </strong> Purchasing products from Etsy.com
 		<p>
-			Description: Sabrina Chavez wants to buy a Free People brand smock dress from Esty.com.</p>
+			<strong>Description:</strong> Sabrina Chavez wants to buy a Free People brand smock dress from Esty.com.</p>
 		<p>
-			Name of User and Their Role: Sabrina Chavez, is a student at UNM and works part-time at Buffalo Exchange.
+			<strong>Name of User and Their Role: </strong>Sabrina Chavez, is a student at UNM and works part-time at Buffalo Exchange.
 		</p>
 		<p>
-			Usage Preconditions: Must have data or internet access via iphone or macbook pro laptop, can have an account, but is not required for purchasing.
+			<strong>Usage Preconditions: </strong> Must have data or internet access via iphone or macbook pro laptop, can have an account, but is not required for purchasing.
 		</p>
 		<p>
-			Usage Postconditions: Product must not be a knock-off, is in good condition upon arrival, and must arrive in a timely manner.
+			<strong> Usage Postconditions:</strong> Product must not be a knock-off, is in good condition upon arrival, and must arrive in a timely manner.
 		</p>
 		<p> <strong>Interaction flow:</strong>
 		</p>
