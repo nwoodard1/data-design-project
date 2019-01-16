7d8c01fe4807e08f3a1e530e5027fa526c360293
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-16 15:44:17 -0700

    added to both use-case and interaction flow files

--- epic/use-case.php
+++ epic/use-case.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8">
 		<title></title>
 	</head>
+
 	<body>
 		<p>
 			Title: Purchasing products from Etsy.com</p>
@@ -18,3 +19,25 @@
 		<p>
 			Usage Postconditions: Product must not be a knock-off, is in good condition upon arrival, and must arrive in a timely manner.
 		</p>
+		<p> <strong>Interaction flow:</strong>
+		</p>
+		<p> <ul>
+					<li>Enter URL or click search results in browser </li>
+					<li>Server recieves http and loads Etsy in browser on customer device </li>
+					<li>Sabrina clicks on "clothing and shoes" option</li>
+					<li>Webpage dropdown shows options for women's clothing and shoes</li>
+					<li>Sabrina clicks link for "Dresses"</li>
+					<li>Server loads new page with images of dresses and filter options</li>
+					<li>Sabrina chooses filters that she wants-size, dress style, pattern and shipping options, etc</li>
+					<li>Server reloads page to incorporate filters automatically</li>
+					<li>Sabrina browses options on page</li>
+					<li>Sabrina chooses size of dress she needs</li>
+					<li> Sabrina clicks button "Add to cart"</li>
+					<li> Server loads cart page</li>
+					<li>Sabrina reviews the item in her cart</li>
+					<li>Sabrina clicks button "pay with Paypal"</li>
+					<li>Server redirects Sabrina to Paypal payment page<li>
+					<li>Sabrina logs into her Paypal account and follows prompts to finish payment and confirm shipping options</li>
+
+		</p>
+	</body>
