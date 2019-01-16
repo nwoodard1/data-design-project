cc1c1b57f16bfba235626334890b645a652c87c8
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-16 14:14:03 -0700

    added technology, attitudes and behaviors, and frustrations paragraphs

--- epic/sabrinachavez.php
+++ epic/sabrinachavez.php
@@ -3,9 +3,15 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Sabrina Chavez</title>
-			<p> Age:25
-			Race: Hispanic/Italian
-			About: Sabrina is a student at UNM and works part-time at Buffalo Exchange. She is passionate about fashion and clothing. She likes brands like Free People, Prada and Pucci.
-			Sabrina spends her free time shopping for deals online and in person.</p>
 	</head>
 	<body>
+		<p> Age:25</p>
+		<p>Race: Hispanic/Italian</p>
+		<p>About: Sabrina is a student at UNM and works part-time at Buffalo Exchange. She is passionate about fashion and clothing. She likes brands like Free People, Prada and Pucci.
+			Sabrina spends her free time shopping for deals online and in person.</p>
+		<p>Technology: Sabrina uses an iPhone 8 and a macbook pro laptop with macOS 10.14 mojave.
+		</p>
+		<p>Attitudes and Behaviors: Sabrina is a very social person, she goes to music festivals and local breweries. She uses Instagram and Snapchat, but doesn't have a Facebook profile.
+		</p>
+		<p>Frustrations: Sabrina wants to be sure that whatever product she is ordering is not a knock-off. Sabrina wants to be sure that the Free People dress she wants to order is in good condition, with no holes or frayed edges. Sabrina wants to be sure that the product is priced lower than the original sale price.
+		</p>
