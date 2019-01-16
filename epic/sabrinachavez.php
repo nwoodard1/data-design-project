6deeae7b5af77b4699b02806af4ad04e50ac9b57
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-16 13:58:21 -0700

    Added new files sabrinachavez.jpg and sabrinachavez.php,added title, age, race and about sabrina for persona profile.

---
+++ epic/sabrinachavez.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Sabrina Chavez</title>
+			<p> Age:25
+			Race: Hispanic/Italian
+			About: Sabrina is a student at UNM and works part-time at Buffalo Exchange. She is passionate about fashion and clothing. She likes brands like Free People, Prada and Pucci.
+			Sabrina spends her free time shopping for deals online and in person.</p>
+	</head>
+	<body>
