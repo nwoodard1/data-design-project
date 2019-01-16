9eb356f30ab2ecc0317de251fd32a1011407c478
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-16 14:59:13 -0700

    added conceptual model file, use-case file and index.html file. Deleted index.php file. Added to index.html file use-case file.

--- epic/sabrinachavez.php
+++ epic/sabrinachavez.php
@@ -2,9 +2,11 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Sabrina Chavez</title>
+		<title></title>
 	</head>
 	<body>
+		<img src="./sabrinachavez.jpg">
+		<h1> Sabrina Chavez </h1>
 		<p> Age:25</p>
 		<p>Race: Hispanic/Italian</p>
 		<p>About: Sabrina is a student at UNM and works part-time at Buffalo Exchange. She is passionate about fashion and clothing. She likes brands like Free People, Prada and Pucci.
@@ -14,4 +16,9 @@
 		<p>Attitudes and Behaviors: Sabrina is a very social person, she goes to music festivals and local breweries. She uses Instagram and Snapchat, but doesn't have a Facebook profile.
 		</p>
 		<p>Frustrations: Sabrina wants to be sure that whatever product she is ordering is not a knock-off. Sabrina wants to be sure that the Free People dress she wants to order is in good condition, with no holes or frayed edges. Sabrina wants to be sure that the product is priced lower than the original sale price.
+		</p>
+		<p>Goals: Sabrina's goal is to find the highest quality Free People smock dress for the cheapest price.
+		</p>
+		<p>
+			User Story: As a user, I need to search for and filter options to find a Free People smock style dress.
 		</p>
---
+++ epic/use-case.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<p>
+			Title: Purchasing products from Etsy.com</p>
+		<p>
+			Description: Sabrina Chavez wants to buy a Free People brand smock dress from Esty.com.</p>
+		<p>
+			Name of User and Their Role: Sabrina Chavez, is a student at UNM and works part-time at Buffalo Exchange.
+		</p>
+		<p>
+			Usage Preconditions: Must have data or internet access via iphone or macbook pro laptop, can have an account, but is not required for purchasing.
+		</p>
+		<p>
+			Usage Postconditions: Product must not be a knock-off, is in good condition upon arrival, and must arrive in a timely manner.
+		</p>
